e532b8466a9d5cfc544e3295a78c87fe78d61749
tstatler <tstatler@cisco.com>
2022-03-31 12:33:25 -0700

    Message app updates

--- browser/messaging/index.js
+++ browser/messaging/index.js
@@ -1,15 +1,116 @@
-let redirect_uri = `${window.location.protocol}//${window.location.host}`;
-
-if (window.location.pathname) {
-  redirect_uri += window.location.pathname;
-}
-console.log(redirect_uri)
+// Get your personal access token at:
+// https://developer.webex.com/docs/getting-started#accounts-and-authentication
+var personal_access_token = "<your_access_token>"
 
 const webex = window.webex = Webex.init({
   credentials: {
-    access_token: `NzZmNmRmNWMtMmQ3Ni00Y2Q1LTljMDAtYWJlM2NlYmY3NzcwYjcyNWI4MWEtZjdk_P0A1_24d5456b-c672-4ced-a782-2192b5ca4d1f`,
+    access_token: personal_access_token,
   },
 });
+
+webex.once(`ready`, function () {
+  var createdRooms;
+  log("Webex ready")
+});
+
+listElement = document.getElementById("list-group");
+var newRooms = [];
+
+function sendMessage() {
+  // send message to createdRoom room
+  var messageText = document.getElementById("message").value;
+  var roomID = document.getElementById("roomid-message").value;
+  // if (newRooms.length) {
+    webex.messages.create({
+      text: messageText,
+      roomId: roomID,
+    }).then(function (message) {
+      log("Message sent to room.")
+    });
+  // }  
+  //  else {
+  //   log("Click 'Create room' and then send the message");
+  // }
+}
+
+function createRoom() {
+  var roomName = document.getElementById("roomName").value;
+  if (roomName == "") {
+    log("Enter a room name.")
+    return;
+  }  
+  webex.rooms.create({ title: roomName }).then(function (room) {
+    newRooms.push({
+      id:   room.id,
+      title: room.title
+    });    
+    log("<i>Created room</i>:" + room.title + " (ID: " + room.id + ")")
+  }).catch(function(error){
+    log(error)
+  });
+
+  // if (createdRoom) {
+  //   log("New room (" + createRoom.title + ") already created. Click Delete Room to create a new room.")
+  //   return;
+  // }
+}
+
+function deleteRoom() {
+  var roomID = document.getElementById("roomid-delete").value;
+
+  if (roomID) {
+    webex.rooms.remove(roomID).then(function () {
+      log("Room deleted")
+      document.getElementById("roomid").value = ""
+    }).catch(function(error) {
+      log(error)
+    })
+  } else {
+    log("Enter the ID of the room to delete.")
+  }
+}
+
+function listRooms() {
+  listElement = document.getElementById("list-group");
+  listElement.innerHTML ="";
+  webex.rooms.list({max: 10}).then(function (rooms) {
+    if (rooms.items.length == 0) {
+      log("<i>There are no rooms. Click 'Create a new room'.</i>");
+    } else {
+      for (var i = 0; i < rooms.items.length; i += 1) {
+        log(rooms.items[i].title + " (ID: " + rooms.items[i].id + ")");
+      }
+    }
+  }).catch(function (error) {
+    log(error);
+  });
+}
+
+function addUser() {
+  var roomID = document.getElementById("roomid-adduser").value;
+  webex.memberships.create({
+     personEmail: 'tim.statler@gmail.com',
+     roomId: roomID
+   }).then(function(membership) {
+      log(membership.personEmail + " added to room");
+   }).catch(function(error) {
+     log(error)
+   });
+}
+
+
+
+// Log helper
+function log(data) {
+  var listItem = document.createElement("li");
+  // listItem.style.display = "block";
+  listItem.classList.add("list-group-item");
+  listItem.innerHTML = data;
+  listElement.prepend(listItem);
+
+}
+
+
 
 // // index.js
 // const webex = window.Webex.init({
@@ -23,92 +124,3 @@ const webex = window.webex = Webex.init({
 //   // },
 // });
 
-listElement = document.getElementById("list-group");
-var createdRoom;
-
-function sendMessage() {
-  // send message to createdRoom room
-  console.log(createdRoom)
-  if (createdRoom) {
-    webex.messages.create({
-      text: "Howdy! https://www.npmjs.com/package/http-server",
-      roomId: createdRoom.id,
-    }).then(function (message) {
-      log("Message sent")
-    });
-  } else {
-    log("Click 'Create room'");
-  }
-}
-
-function createRoom() {
-  if (createdRoom) {
-    log("New room (" + createRoom.title + ") already created. Click Delete Room to create a new room.")
-    return;
-  }
-  var roomName = document.getElementById("roomName").value;
-  // alert(roomName)
-  if (roomName == "") {
-    log("Please enter a room name.")
-    return;
-  }
-
-  webex.rooms.create({ title: roomName }).then(function (room) {
-    createdRoom = room;
-    log("<i>Created room</i>:" + room.title)
-  });
-}
-
-function deleteRoom() {
-  if (createdRoom) {
-    webex.rooms.remove(createdRoom.id).then(function () {
-      log("Room deleted")
-      createdRoom = null;
-    })
-  } else {
-    log("Nothing to delete")
-  }
-}
-
-function listRooms() {
-  console.log("webex: " + webex);
-  listElement = document.getElementById("list-group");
-  listElement.innerHTML = "";
-
-  webex.rooms.list().then(function (rooms) {
-    if (rooms.items.length == 0) {
-      var listItem = document.createElement("li");
-      listItem.style.display = "block";
-      listItem.classList.add("list-group-item");
-      listItem.innerHTML =
-        "<i>There are no rooms. Click 'Create a new room'.</i>";
-      listElement.appendChild(listItem);
-    } else {
-      for (var i = 0; i < rooms.items.length; i += 1) {
-        log("<i>Created room: </i>: " + rooms.items[i].title);
-        // var listItem = document.createElement("li");
-        // listItem.classList.add("list-group-item");
-        // listItem.innerHTML = "<i>Created room: </i>: " + rooms.items[i].title;
-        // listElement.appendChild(listItem);
-      }
-    }
-  });
-}
-
-webex.once(`ready`, function () {
-  var createdRooms;
-  // listRooms();
-});
-
-function log(data) {
-  // var li = document.createElement("li");
-  // var payload = document.createTextNode(JSON.stringify(data));
-  // li.appendChild(payload)
-  // listElement.appendChild(li);
-  var listItem = document.createElement("li");
-  listItem.style.display = "block";
-  listItem.classList.add("list-group-item");
-  listItem.innerHTML = data;
-  listElement.appendChild(listItem);
-
-}
